6130d35b75b011edf42a613ea986f4d6ee2b5856
Gokay <gkysrt@gmail.com>
2020-08-10 00:07:53 +0300

    Changes: - Delegate class implementation

--- src/main/python/delegates/ListViewDelegate.py
+++ src/main/python/delegates/ListViewDelegate.py
@@ -6,10 +6,18 @@ class ListViewDelegate(QtWidgets.QStyledItemDelegate):
 	def __init__(self, parent = None):
 		super().__init__(parent)
 		self.__nameRect = QtCore.QRect()
+
+		self.__topMargin = 5
+		self.__bottomMargin = 5
+		self.__leftMargin = 5
+		self.__rightMargin = 5
+		self.__spacing = 5
+
+		self.__topLeftMarginPoint = QtCore.QPoint(self.__leftMargin, self.__topMargin)
+
 		self.__evenColor = QtGui.QColor(64, 64, 64)
 		self.__oddColor = QtGui.QColor(80, 80, 80)
 		self.__textColor = QtGui.QColor(200, 200, 200)
-		self.__topLeftMargin = QtCore.QPoint(5, 5)
 
 	def paint(self, painter, option, index):
 		super().paint(painter, option, index)
@@ -31,13 +39,41 @@ class ListViewDelegate(QtWidgets.QStyledItemDelegate):
 		isEvenNr = True if (row % 2) == 0 else False
 		painter.fillRect(rectangle, self.__evenColor) if isEvenNr else painter.fillRect(rectangle, self.__oddColor)
 
-		iconRect = QtCore.QRect(option.rect.topLeft() + self.__topLeftMargin, QtCore.QPoint(option.rect.width() - 10, option.rect.y() + option.rect.height() / 2))
-		nameRect = QtCore.QRect(option.rect.topLeft() + self.__topLeftMargin, QtCore.QPoint(option.rect.width() - 10, option.rect.y() + option.rect.height() / 2))
+		iconRect = QtCore.QRect(option.rect.topLeft() + self.__topLeftMarginPoint, QtCore.QPoint(option.rect.height() - self.__bottomMargin - self.__topMargin, option.rect.y() + option.rect.height() - self.__bottomMargin))
+		nameRect = QtCore.QRect(QtCore.QPoint(iconRect.topRight().x() + self.__spacing, iconRect.topRight().y()), QtCore.QPoint(option.rect.width() - self.__rightMargin, option.rect.y() + option.rect.height() / 2))
+		macRect = QtCore.QRect()
+		connectionRect = QtCore.QRect()
+
 		print(option.rect)
 		print(nameRect)
 
 		painter.drawText(nameRect, 0, fontMetrics.elidedText(deviceName, QtCore.Qt.ElideRight, nameRect.width()))
+
+		painter.drawText(iconRect, 0, "icon\nhere")
+		painter.drawRect(iconRect)
+
 		painter.restore()
 
 	def sizeHint(self, option, index):
 		return QtCore.QSize(option.rect.width(), 60)
+
+	def setContentMargins(self, left, top, right, bottom):
+		self.__leftMargin = int(left)
+		self.__topMargin = int(top)
+		self.__rightMargin = int(right)
+		self.__bottomMargin = int(bottom)
+
+	def contentMargins(self):
+		return self.__leftMargin, self.__topMargin, self.__rightMargin, self.__bottomMargin
+
+	def setSpacing(self, spacing):
+		self.__spacing = int(spacing)
+
+	def spacing(self):
+		return self.__spacing
+
+	def setPrimaryBackgroundColor(self, color):
+		self.__evenColor = color
+
+	def setSecondaryBackgroundColor(self, color):
+		self.__oddColor = color
